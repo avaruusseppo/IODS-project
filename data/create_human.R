084dc7eeafbcc6439aa33e228d649f89e6d0f73a
Seppo Nyrkkö <seppo.nyrkko@gmail.com>
2020-11-26 23:30:25 +0200

    Wrangling for chapter 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,5 @@
-# Seppo Nyrkkö Nov 20, 2020
-# Exercise 4 - Data wrangling with the human development and equality data
+# Seppo Nyrkkö Nov 20, 2020 + Nov 27, 2020
+# Exercise 4 + 5 - Data wrangling with the human development and equality data
 
 # The data
 
@@ -70,3 +70,82 @@ str(humantest)
 dim(humantest)
 
 # ok!
+
+# Part 5: continuing... Nov 27 2020
+
+# Loaded the ‘human’ data into R. 
+
+str(human)
+cat(names(human))
+
+# This data set has has columns:
+# hdirank country hdi lifeexp eduexpy edumeany gni gnihdidiff giirank
+# gii matmort adolbirth parlrepr f2edu m2edu flabor mlabor ratiofm2edu ratiofmlabor
+#
+# combined from the Human development and Gender inequality statistics per country
+
+
+# Mutate the data: transform the Gross National Income (GNI) variable to numeric 
+# (Using string manipulation. Note that the mutation of 'human' was not done on DataCamp).
+# (1 point)
+
+str(human$gni)
+as.numeric(gsub(',','',human$gni)) -> human$gni
+str(human$gni)
+
+# Exclude unneeded variables: keep only the columns matching the following variable names
+# (described in the meta file above):  
+# "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", 
+# "Mat.Mor", "Ado.Birth", "Parli.F"
+
+keep_columns <- c("country", "ratiofm2edu", "ratiofmlabor", "eduexpy",
+                  "lifeexp", "gni", "matmort", "adolbirth", "parlrepr")
+
+names(human)
+
+# select the 'keep_columns' to create a new dataset
+library(tidyr)
+library(dplyr)
+
+human1 <-  dplyr::select(human,one_of(keep_columns))
+
+# look at the summaries and structure of the data
+summary(human1)
+str(human1)
+
+# Remove all rows with missing values...
+human2 <- na.omit(human1)
+str(human2)
+# reduced to 162 obs and 9 variables, ok!
+
+# Remove the observations which relate to regions ... not countries
+
+human2$country
+
+# The last 7 contains regions data, rows 1...155 are countries
+
+human3 <- human2[1:155,]
+
+str(human3)
+
+# ok
+
+human4 <- human3[,colnames(human3) != 'country']
+rownames(human4) <- human3$country
+
+# Define the row names of the data by the country names 
+# and remove the country name column from the data.
+
+str(human4)
+
+# The data should now have 155 observations and 8 variables
+# All OK.
+
+# Save the human data again
+write.csv(human4, "human.csv", row.names = T)
+
+
+
+
+
+
