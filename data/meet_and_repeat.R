a825c9fdbcedf117924a79646e8df416c8958de0
Seppo Nyrkkö <seppo.nyrkko@softwarepoint.com>
2020-12-03 15:48:37 +0200

    Chapter 6 stub

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,12 @@
+# Seppo Nyrkkö Dec 4, 2020
+# IODS Exercise 6 - Data wrangling for longitudinal analysis
+
+rats <- read.table("rats.txt")
+
+bprs <- read.table("BPRS.txt")
+
+str(rats)
+
+str(bprs)
+
+
